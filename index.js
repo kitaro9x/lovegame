269d14a47f2f98bb25b2f377ce7f5a0074ea3ecc
kitaro9x <91399990+kitaro9x@users.noreply.github.com>
2022-02-24 09:54:08 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1
+var randomImageLink1 = "images/dice" + randomNumber1 +".png"
+var randomImageSource1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageLink1)
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1
+var randomImageLink2 = "images/dice" + randomNumber2 +".png"
+var randomImageSource2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageLink2)
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerText = "Xuxumint Win"
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerText = "Bibimint Win"
+} else {
+    document.querySelector("h1").innerText = "Two player Draw"
+}
