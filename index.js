f9791babf9dd278b07e1067e9852a994e80aeffd
kitaro9x <91399990+kitaro9x@users.noreply.github.com>
2022-02-24 09:58:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var randomNumber1 = Math.floor(Math.random()*6) + 1
-var randomImageLink1 = "images/dice" + randomNumber1 +".png"
+var randomImageLink1 = "dice" + randomNumber1 +".png"
 var randomImageSource1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageLink1)
 
 var randomNumber2 = Math.floor(Math.random()*6) + 1
-var randomImageLink2 = "images/dice" + randomNumber2 +".png"
+var randomImageLink2 = "dice" + randomNumber2 +".png"
 var randomImageSource2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageLink2)
 
 if(randomNumber1 > randomNumber2) {
